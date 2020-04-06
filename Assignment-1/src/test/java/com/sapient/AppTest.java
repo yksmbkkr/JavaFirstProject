0f6d220290f5bd5502b382d23bef0e4ae26fef0b
Yash Kulshreshtha <yk.smb.kkr@gmail.com>
2020-04-06 13:46:00 +0530

    Adding tests

--- Assignment-1/src/test/java/com/sapient/AppTest.java
+++ Assignment-1/src/test/java/com/sapient/AppTest.java
@@ -1,38 +1,83 @@
 package com.sapient;
 
-import junit.framework.Test;
+// import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+// import junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+// public class AppTest 
+//     extends TestCase
+// {
+//     /**
+//      * Create the test case
+//      *
+//      * @param testName name of the test case
+//      */
+//     public AppTest( String testName )
+//     {
+//         super( testName );
+//     }
+
+//     /**
+//      * @return the suite of tests being tested
+//      */
+//     public static Test suite()
+//     {
+//         return new TestSuite( AppTest.class );
+//     }
+
+//     /**
+//      * Rigourous Test :-)
+//      */
+//     public void testApp()
+//     {
+//         assertTrue( true );
+//     }
+// }
+
+public class AppTest {
+    private CheckEven app;
+    
+    @Before
+    public void setup() {
+        app = new CheckEven();
+    }
+    
+    
+    @Test
+    public void TestZeroAsEven() {
+        boolean flag = app.check(0);
+        assertTrue(flag == true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void Test2Case() {
+        boolean flag = app.check(90);
+        assertTrue(flag == true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void Test3Case() {
+        boolean flag = app.check(101);
+        assertTrue(flag == false);
+    }
+
+    @Test
+    public void Test4Case() {
+        boolean flag = app.check(2);
+        assertTrue(flag == true);
+    }
+
+    @Test
+    public void Test5Case() {
+        boolean flag = app.check(89);
+        assertTrue(flag == false);
     }
 }
