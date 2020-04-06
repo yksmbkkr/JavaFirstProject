aaeeb122fa7cfc50b72230e1c882f49878e20035
Yash Kulshreshtha <yk.smb.kkr@gmail.com>
2020-04-06 13:20:43 +0530

    Added check even class and function

--- Assignment-1/src/main/java/com/sapient/App.java
+++ Assignment-1/src/main/java/com/sapient/App.java
@@ -1,4 +1,6 @@
 package com.sapient;
+import java.util.Scanner;
+import com.sapient.CheckEven;
 
 /**
  * Hello world!
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Enter A Integer > " );
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if(CheckEven.check(number)){
+            System.out.println("Its an even number");
+        }else{
+            System.out.println("Its an odd number");
+        }
     }
 }
---
+++ Assignment-1/src/main/java/com/sapient/CheckEven.java
@@ -0,0 +1,10 @@
+package com.sapient;
+
+public class CheckEven {
+    public static boolean check(int n){
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+}
