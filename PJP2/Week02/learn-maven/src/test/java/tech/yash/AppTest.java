8c9f6a35c95f60918566bea36116c68a6b614064
Yash Kulshreshtha <yk.smb.kkr@gmail.com>
2020-08-18 20:16:41 +0530

    Initializing Maven Quickstart Project

---
+++ PJP2/Week02/learn-maven/src/test/java/tech/yash/AppTest.java
@@ -0,0 +1,20 @@
+package tech.yash;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
